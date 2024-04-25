64840d460b032e2de47c9cd9192b4bd34cb36e2f
Joaoojohn <joaopedro.cs12@hotmail.com>
2024-04-25 01:48:22 -0300

    data row to testing multiples  url's

--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -11,15 +11,45 @@ namespace UtmBuilder.Core.Tests.ValueObjects
     [TestClass]
     public class UrlTests
     {
+        private const string _InvalidUrl = "SociedadeEsportivaPalmeiras";
+        private const string _ValidUrl = "https://www.palmeirasstore.com/";
+
         [TestMethod]
         [ExpectedException(typeof(InvalidUrlException))]
-        public void GivenAInvalidURLShouldReturnAnException() => new Url("SociedadeEsportivaPalmeiras");
+        public void GivenAInvalidURLShouldReturnAnException() => new Url(_InvalidUrl);
 
         [TestMethod]
         public void GivenAValidURLShouldNotThrowException()
         {
-            new Url("https://www.palmeirasstore.com/");
+            new Url(_ValidUrl);
             Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        [DataRow(" ", true)]
+        [DataRow("http", true)]
+        [DataRow("Palmeiras", true)]
+        [DataRow("https://www.palmeirasstore.com/", false)]
+        public void TestingMultipleURLs(string link, bool expectedException)
+        {
+            if (expectedException)
+            {
+                try
+                {
+                    new Url(link);
+
+                    Assert.Fail();
+                }
+                catch (InvalidUrlException)
+                {
+                    Assert.IsTrue(true);
+                }
+            }
+            else
+            {
+                new Url(link);
+                Assert.IsTrue(true);
+            }
         }
     }
 }
